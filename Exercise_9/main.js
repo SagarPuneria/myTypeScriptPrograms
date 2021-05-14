18f40d8edfa0c7481f5460f2893eeee41aac6584
Sagar Puneria <Sagar.Puneria@gmail.com>
2021-05-14 15:47:10 +0530

    Code refactor

--- Exercise_9/main.js
+++ Exercise_9/main.js
@@ -2,7 +2,5 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var like_component_1 = require("./like.component");
 var component = new like_component_1.LikeComponent(10, true);
-// component._likesCount = 2; // Ideally likesCount value should private and only be changed inside onClick() method
-// component._isSelected = false;  // Ideally isSelected value should private and only be changed inside onClick() method
 component.onClick();
 console.log("likesCount: " + component.likesCount + ", isSelected: " + component.isSelected);
